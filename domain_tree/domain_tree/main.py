f9920b4d651e3751f929aafd79dece8575c1abef
Vincenzo Maria Stanzione <50915778+virtualms@users.noreply.github.com>
2021-11-25 14:52:37 +0100

    test integration end __eq__

---
+++ domain_tree/domain_tree/main.py
@@ -0,0 +1,92 @@
+import random
+
+from tree import DomainTree
+from scipy.stats import qmc
+import altair as alt
+import numpy as np
+import pandas as pd
+import webbrowser
+import time
+from domain import RealDomain, RealInterval
+
+# (n+1)/2 leaves
+DEPTH = 5
+NODE_NAME = "node"
+K = 1
+VARIABLES = ["x" + str(i) for i in range(K)]
+DOMAINS = RealDomain({val: RealInterval((0, 1), (True, True)) for i, val in enumerate(VARIABLES)})
+x = {v: 0.5 for v in VARIABLES}
+RANDOM = 0 # [0, 1]
+MIN_SPLIT = 0.1 # [0, 0.5]
+COEF_BOUNDS = (-5, 5)
+
+
+def sanity_check1d(leaves):
+    def correct_domain(leaf, next_leaf):
+        problematic = ""
+        correct = True
+        for var in leaf.variables:
+            # print(f"{leaf.domains[var][0]} == {next_leaf.domains[var][1]} or {leaf.domains[var][1]} == {next_leaf.domains[var][0]} or {leaf.domains[var]} == {next_leaf.domains[var]}?")
+            if not (leaf.domains[var][0] == next_leaf.domains[var][
+                1]):  # or leaf.domains[var][1] == next_leaf.domains[var][0] or leaf.domains[var] == next_leaf.domains[var]):
+                correct = False
+                problematic = var
+                break
+
+        return correct, problematic
+
+    for i, leaf in enumerate(leaves):
+        if i == len(leaves) - 1:
+            break
+
+        print(i)
+        next_leaf = leaves[i + 1]
+
+        correct, var = correct_domain(leaf, next_leaf)
+        if not correct:
+            raise Exception(
+                f"Incorrect domains found. Leaf: {leaf.name} {leaf.domains} and Leaf: {next_leaf.name} {next_leaf.domains}.: . Mismatch on {var}")
+
+
+def sobol(display=False, m=6):
+    sampler = qmc.Sobol(d=K, scramble=False)
+    X = sampler.random_base2(m=m)
+    y = [tree.compute_f({var: x[i] for i, var in enumerate(VARIABLES)}) for x in X]
+
+    if display:
+        chart = tree.visualize_regressions(mode="none")
+        df = pd.DataFrame(np.c_[X, y], columns=["x", "y"])
+        fig = alt.Chart(df).mark_circle(size=20).encode(
+            x=alt.X("x", scale=alt.Scale(zero=False), type="quantitative"),
+            y=alt.Y("y", scale=alt.Scale(zero=False), type="quantitative"),
+            color=alt.value("yellow"),
+            tooltip=["x", "y"]
+        )
+        final = alt.layer(chart, fig).properties(width=600, height=500).interactive()
+        # fname = "./imgs/final.html"
+        # final.save(fname)
+        #webbrowser.open_new_tab("C:\\_git\\ds_tests\\GlobalLime\\TreeDomain\\imgs\\final.html")
+        final.show(open_browser=True)
+
+
+def main():
+    global tree
+    tree = DomainTree(domains=DOMAINS,
+                      name=NODE_NAME,
+                      min_split=MIN_SPLIT,
+                      depth_max=DEPTH,
+                      random=RANDOM,
+                      coef_bounds=COEF_BOUNDS)
+    print("Tree built!")
+    tree.visualize_all_domains()
+    # tree.visualize_domains("x0", "x1")
+    tree.print_tree()
+    tree.render_tree()
+    #print(x)
+    print(tree.compute_f(x))
+    #print(tree.contains(x))
+    sobol(display=True, m=6)
+
+
+if __name__ == '__main__':
+    main()
---
+++ domain_tree/test/test_kpi.py
@@ -0,0 +1,99 @@
+# import sys
+# sys.path.append("..\\domain_tree\\")
+import unittest
+from domain_tree.tree import DomainTree, DomainNode, NodeNotFoundException
+from domain_tree import kpi
+from domain_tree.domain import RealDomain, RealInterval
+
+
+class TestKPI(unittest.TestCase):
+
+    def setUp(self):
+        self.depth = 3
+        self.d0 = RealDomain({"x0": RealInterval((0, 1), (True, False)), "x1": RealInterval((0, 1), (True, False))})
+        self.d1 = RealDomain({"x0": RealInterval((1, 2), (True, False)), "x1": RealInterval((1, 2), (True, False))})
+        self.original_m = DomainTree(self.d0, depth_max=self.depth)
+        self.approx_m = DomainTree(self.d0, depth_max=self.depth)
+        self.blank_m = DomainTree(self.d0, min_split=1, depth_max=self.depth)
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_check_partition_num(self):
+        original_partitions, approx_partitions, delta_p = kpi.check_partition_num(self.original_m, self.original_m)
+        self.assertEqual(original_partitions, approx_partitions)
+        self.assertEqual(delta_p, 0)
+
+        original_partitions, approx_partitions, delta_p = kpi.check_partition_num(self.original_m, self.blank_m)
+        self.assertNotEqual(original_partitions, approx_partitions)
+        self.assertEqual(delta_p, -len(self.original_m.leaves) + 1)
+
+    @unittest.skip("check_r2 is fake, waiting for the real model")
+    def test_check_r2(self):
+        pass
+
+    def test_matching_bounds(self):
+        a = (0, 1)
+        b = (0, 1)
+        self.assertTrue(kpi.matching_bounds(a, b, 0))
+
+        a = (0, 1)
+        b = (0, 1.01)
+        self.assertFalse(kpi.matching_bounds(a, b, 0))
+
+        a = (0, 1)
+        b = (-0.5, 0.5)
+        self.assertTrue(kpi.matching_bounds(a, b, 0.5))
+        self.assertTrue(kpi.matching_bounds(b, a, 0.5))
+        self.assertFalse(kpi.matching_bounds(a, b, 0.4999))
+        self.assertFalse(kpi.matching_bounds(b, a, 0.4999))
+
+        a = (0.3, 0.6)
+        b = (0.2, 0.7)
+        self.assertTrue(kpi.matching_bounds(a, b, 0.1))
+        self.assertTrue(kpi.matching_bounds(b, a, 0.1))
+        self.assertFalse(kpi.matching_bounds(a, b, 0))
+        self.assertFalse(kpi.matching_bounds(b, a, 0))
+
+
+        #float precision error
+        a = (0.4, 0.6)
+        b = (0.3, 0.7)
+        self.assertTrue(kpi.matching_bounds(a, b, 0.1))
+
+        a = (-0.3, -0.6)
+        b = (-0.2, -0.7)
+        self.assertTrue(kpi.matching_bounds(a, b, 0.1))
+        self.assertTrue(kpi.matching_bounds(b, a, 0.1))
+        self.assertFalse(kpi.matching_bounds(a, b, 0))
+        self.assertFalse(kpi.matching_bounds(b, a, 0))
+
+
+
+    def test_matching_intervals(self):
+        #TODO very naive
+        self.assertFalse(kpi.matching_intervals(DomainNode(domains=self.d0, name="node"),
+                                                DomainNode(domains=self.d1, name="node"), 0))
+        self.assertTrue(kpi.matching_intervals(DomainNode(domains=self.d0, name="node"),
+                                               DomainNode(domains=self.d1, name="node"), 1))
+        self.assertTrue(kpi.matching_intervals(DomainNode(domains=self.d0, name="node"),
+                                               DomainNode(domains=self.d0, name="node"), 0))
+
+    def test_check_matching_partitions(self):
+        # conf=0
+        part_num = len(self.original_m.leaves)
+        self.assertEqual(kpi.check_matching_partitions(self.original_m, self.original_m, n=1, d=0), part_num)
+        self.assertEqual(kpi.check_matching_partitions(self.original_m, self.blank_m, n=1, d=0), 0)
+
+        # conf=1
+        self.assertEqual(kpi.check_matching_partitions(self.original_m, self.approx_m, n=1, d=4), part_num)
+        self.assertEqual(kpi.check_matching_partitions(self.original_m, self.blank_m, n=1, d=4), 1)
+
+        # conf=0.5
+        orig = DomainTree(self.d0, min_split=0.25, depth_max=3)
+        appr = DomainTree(self.d0, min_split=0.25, depth_max=3)
+        self.assertIn(kpi.check_matching_partitions(orig, appr, n=2, d=4), [part_num - 2, part_num - 1, part_num])
+
+
+if __name__ == "__main__":
+    unittest.main()
